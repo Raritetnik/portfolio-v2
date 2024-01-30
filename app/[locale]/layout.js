0ca651ecef584a8b26f38f9cb40cd629f7b316c3
Raritetnik <mkodarenko@gmail.com>
2024-01-30 10:01:04 -0500

    No jumping content

--- app/[locale]/layout.js
+++ app/[locale]/layout.js
@@ -10,8 +10,13 @@ import translations from "@/messages/en.json";
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Mykhael Kuzmin | Full Stack Web Developer',
-  description: 'Mykhael Kuzmin | Full Stack Web Developer',
+  title: 'Mykhaylo Kuzmin | Full Stack Web Developer',
+  description: 'Un professionnel en développement Full Stack de ' +
+      'sites web basé à Montréal, Québec. Conception de designs, ' +
+      'modélisation et structuration de bases de données, création et ' +
+      'programmation des aspects serveur et de l\'interface utilisateur, ' +
+      'amélioration du référencement (SEO) et application des ' +
+      'meilleures pratiques de développement.',
 }
 
 export default function RootLayout({ children, params }) {
